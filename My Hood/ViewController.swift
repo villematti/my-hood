43f93ac2e196684410b770f5ccdb9adce1f682ac
Ville-Matti Hakanpää <villematti@villematti.info>
2016-01-07 20:10:38 +0200

    Initial Commit

---
+++ My Hood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My Hood
+//
+//  Created by Ville-Matti Hakanpää on 07/01/16.
+//  Copyright © 2016 Ville-Matti Hakanpää. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
